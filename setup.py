910eceacc530a4b540cc3103e7650ddf1c81135e
Olegas Domanskis <olegas@domanskis.eu>
2021-10-03 16:51:52 +0300

    renaming, adding code from aam plugin

--- setup.py
+++ setup.py
@@ -6,23 +6,23 @@ requirements = []  # add Python dependencies here
 # e.g., requirements = ["PyYAML"]
 
 setup(
-    name='awx-custom-credential-plugin-example',
+    name='db-cyberark-aam',
     version='0.1',
-    author='Ansible, Inc.',
-    author_email='info@ansible.com',
+    author='Danske Bank, A/S',
+    author_email='odo@danskebank.lt',
     description='',
     long_description='',
     license='Apache License 2.0',
     keywords='ansible',
     url='http://github.com/ansible/awx-custom-credential-plugin-example',
-    packages=['awx_custom_credential_plugin_example'],
+    packages=['db-cyberark-aam'],
     include_package_data=True,
     zip_safe=False,
     setup_requires=[],
     install_requires=requirements,
     entry_points = {
         'awx.credential_plugins': [
-            'example_plugin = awx_custom_credential_plugin_example:example_plugin',
+            'db-cyberark-aam =   db-custom-aam:db-cyberark-aam',
         ]
     }
 )
